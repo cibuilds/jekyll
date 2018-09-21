e6279341b1a3db73af53a02f1b6cb8d99f339108
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-09-21 10:15:55 -0400

    Add Jekyll v3.8.4. (#1)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 3.8/Dockerfile -t cibuilds/jekyll:latest -t cibuilds/jekyll:3.8.3  -t cibuilds/jekyll:3.8 .
+docker build --file 3.8/Dockerfile -t cibuilds/jekyll:latest -t cibuilds/jekyll:3.8.4  -t cibuilds/jekyll:3.8 .
